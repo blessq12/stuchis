ac4cd1cabbe6c5edfaa3f7d363a5ca4044a31b1c
Andrew <code70@inbox.ru>
2023-04-03 14:35:29 +0700

    debugbar,routest domain group,error handling

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::domain(env('APP_URL'))->group(function(){
+    Route::get('/',function(){
+        return view('welcome');
+    });
 });
