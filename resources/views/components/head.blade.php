f34d5bc7e8eab40e7a1eb8a4e89c25f44b1c40ac
Andrew <code70@inbox.ru>
2023-04-19 18:13:28 +0700

    style version

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -28,7 +28,7 @@
     <link rel="stylesheet" href="{{ asset('assets/css/vendor/slick-theme.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/vendor/base.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/plugins/plugins.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css?v=1') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css?v=11') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/user.css?v=1.11') }}">
 
 </head>
