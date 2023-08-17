ce049f34605e5885bbddc2b24f4b28cfa24d4d7e
Andrew <code70@inbox.ru>
2023-08-17 13:17:23 +0700

    reset

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,14 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\CarpetsType;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
     public function carpets(){
-        $carpet = CarpetsType::find(9);
-        $carpet_height = $carpet->getHeight();
-        return response()->json($carpet_height);
+        
     }
 }
