9a0daf20e9171579513dae4589a63731316360aa
Andrew <code70@inbox.ru>
2023-08-20 19:36:38 +0700

    web and api redirect

--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::domain('stuchis.ru')->group(function(){
-    Route::get('{any}',function($any){
-        return redirect('http://fskanapa.ru/api/'.$any , 301);
-    })->where('any', '.*');
-});
-
 Route::controller(ApiController::class)->group(function(){
     Route::get('/carpets','carpets');
     Route::post('/send','send');
